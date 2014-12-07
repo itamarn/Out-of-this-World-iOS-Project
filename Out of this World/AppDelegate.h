2cc81eb12dd1418246d82e20e940b7af1de8de85
Itamar Nakar <user@MacBook-Pro.local>
2014-12-07 12:53:09 +0200

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by Itamar Nakar on 07/12/14.
+//  Copyright (c) 2014 Itamar Nakar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of this World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of this World
+//
+//  Created by Itamar Nakar on 07/12/14.
+//  Copyright (c) 2014 Itamar Nakar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
